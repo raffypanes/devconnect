b67cea971f4ac6e91387048ec5dbd5848085bbde
raffypanes <raffy_panes@hotmail.com>
2018-07-13 17:52:35 +0800

    Updated Users API

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 
 // Load User model
 const User = require('../../models/User');
@@ -17,11 +19,27 @@ router.post('/register', (req, res) => {
     if (user) {
       return res.status(400).json({ email: 'Email already exist' });
     } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: '200', // image size
+        r: 'pg', // image rating
+        d: 'mm', // default
+      });
       const newUser = new User({
         name: req.body.name,
         email: req.body.email,
         avatar,
         password: req.body.password,
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
       });
     }
   });
